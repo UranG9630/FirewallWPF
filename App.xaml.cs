42cd76c804e3da32ee2db55518f53d1ff13d292e
Kajetan Kaczmarek <urang9630@gmail.com>
2026-08-08 20:55:59 +0200

    Initial commit with template WPF app

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FirewallWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
